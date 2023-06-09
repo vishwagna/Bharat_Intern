ef670507478ba9fb869905c5937b408f0b883a94
vishwagna <106877724+vishwagna@users.noreply.github.com>
2023-06-09 15:57:36 +0530

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost:3306";
-    $username = "root";
-    $password = "";
-    $dbname = "intern";
-    $conn = new mysqli($servername, $username, $password,$dbname);
-    ?>
